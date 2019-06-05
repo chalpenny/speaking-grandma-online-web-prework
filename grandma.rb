6e020eee18d1658597a199c1be80dee55f64424e
elegant-intelligence-1985 <cp.halpenny@gmail.com>
2019-06-05 04:53:54 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,7 @@
 def speak_to_grandma(phrase)
 if phrase == "I LOVE YOU GRANDMA!"
   return "I LOVE YOU TOO PUMPKIN!"
-elsif phrase == "phrase".upcase
+elsif phrase.upcase == phrase
   return "NO, NOT SINCE 1938!"
 else
   "HUH? SPEAK UP SONNY!"
